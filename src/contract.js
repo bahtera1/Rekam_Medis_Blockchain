f5c043ceccea9f68c12d3ba97fa074e6325ad75a
Naufalas <naufalaja080@gmail.com>
2025-06-27 20:31:48 +0700

    mengubah contract address ke environment

--- src/contract.js
+++ src/contract.js
@@ -2,7 +2,13 @@ import web3 from "./web3";
 // import ABI dari folder abis
 import RekamMedisRS from "./abis/RekamMedisRS.json";
 
-const address = "0x6dEf3740b6CB81c31d251cA3d31881BbBd90BE1a";
+const address = process.env.REACT_APP_CONTRACT_ADDRESS;
+
+// Pastikan alamat kontrak telah dimuat
+if (!address) {
+    console.error("Alamat kontrak tidak ditemukan. Pastikan Anda telah mengatur REACT_APP_CONTRACT_ADDRESS di file .env");
+}
+
 const contract = new web3.eth.Contract(RekamMedisRS.abi, address);
 
 export default contract;
