7a15282e7471a013669ef07e086c5901fff8044d
Naufalas <naufalaja080@gmail.com>
2025-07-24 18:19:00 +0700

    tes

--- src/PinataUpload.js
+++ src/PinataUpload.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const PINATA_JWT = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiJhYWFjNmUyNi1mNjJiLTQ1NjUtOTE0Ny0zZjEzMGJjZTZkZjQiLCJlbWFpbCI6Im5hdWZhbGFqYTA4MEBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwicGluX3BvbGljeSI6eyJyZWdpb25zIjpbeyJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MSwiaWQiOiJGUkExIn0seyJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MSwiaWQiOiJOWUMxIn1dLCJ2ZXJzaW9uIjoxfSwibWZhX2VuYWJsZWQiOmZhbHNlLCJzdGF0dXMiOiJBQ1RJVkUifSwiYXV0aGVudGljYXRpb25UeXBlIjoic2NvcGVkS2V5Iiwic2NvcGVkS2V5S2V5IjoiYTU4OTZmMWIyYjBkYTVlZTU2YTAiLCJzY29wZWRLZXlTZWNyZXQiOiJjMDFiYjZmMjAxYzE4OWI1YzIwZjY0ZTQyZWRhZDU4ZjVhYmJkMTk5NDAyMjhhMTMzMWFhN2Y4Mjk4YmEyMTIyIiwiZXhwIjoxNzc5NjQyMjMyfQ.Tsg3OrfSfX9c-9D10aw6aVRoA9BZ4dWZm-SwzFvV8-s"; // prepend 'Bearer '
+// Mengambil token dari environment variable
+const PINATA_JWT = process.env.REACT_APP_PINATA_JWT;
 
 export async function uploadToPinata(file) {
     const url = `https://api.pinata.cloud/pinning/pinFileToIPFS`;
@@ -18,6 +19,7 @@ export async function uploadToPinata(file) {
         const ipfsHash = res.data.IpfsHash;
         return `https://gateway.pinata.cloud/ipfs/${ipfsHash}`;
     } catch (err) {
+        console.error("Upload to Pinata failed:", err);
         throw err;
     }
 }
